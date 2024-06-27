51d4f56066a1c639326e2875ccada56281a95084
abamka <Januska12>
2024-06-27 14:36:47 +0300

    module: 2 task

---
+++ JS_Practice_React/image_fetch_2/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import RandomDogImage from './RandomImage';
+
+function App() {
+    return (
+        <div className="App">
+            <RandomDogImage />
+        </div>
+    );
+}
+
+export default App;

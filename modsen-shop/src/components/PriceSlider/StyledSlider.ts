c1920f1b712ea2480e5b9f2e939ea06181b850f1
abamka <Januska12>
2024-07-01 14:57:16 +0300

    fix: price slider

--- modsen-shop/src/components/PriceSlider/StyledSlider.ts
+++ modsen-shop/src/components/PriceSlider/StyledSlider.ts
@@ -1,39 +1,45 @@
 import '@/styles/variables.css';
 
-import Slider from 'react-slider';
 import styled from 'styled-components';
 
-const StyledSlider = styled(Slider)`
-  height: 2px;
-  margin: 20px auto;
+const StyledSlider = styled.div`
   position: relative;
-  background: ${({ theme }) => theme.colors.gray};
-
-  .track {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    background: ${({ theme }) =>
-      theme.title === 'dark' ? theme.colors.darkGray : theme.colors.gray};
-  }
-
-  .track-1 {
-    background: ${({ theme }) => theme.colors.text};
-  }
-
-  .thumb {
-    height: 10px;
-    width: 2px;
-    cursor: pointer;
-    position: absolute;
-    top: 1px;
-    margin-top: -5px;
-  }
+  width: 100%;
 `;
 
-const ValueDisplay = styled.div`
-  text-align: center;
-  margin-top: 10px;
+const SliderTrack = styled.div`
+  position: relative;
+  height: 4px;
+  background: ${({ theme }) => theme.colors.gray};
+  margin: 20px 0;
+`;
+
+const SliderThumb = styled.input`
+  position: absolute;
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 4px;
+  background: transparent;
+  pointer-events: none;
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 3px;
+    height: 16px;
+    background: ${({ theme }) => theme.colors.text};
+    cursor: pointer;
+    pointer-events: auto;
+  }
+
+  &::-moz-range-thumb {
+    width: 16px;
+    height: 16px;
+    background: ${({ theme }) => theme.colors.text};
+    cursor: pointer;
+    pointer-events: auto;
+  }
 `;
 
 const Wrapper = styled.div`
@@ -49,4 +55,10 @@ const Price = styled.div`
   text-align: left;
 `;
 
-export { Price, StyledSlider, ValueDisplay, Wrapper };
+const ValueDisplay = styled.div`
+  text-align: center;
+  margin-top: 10px;
+`;
+
+export { Price, StyledSlider, ValueDisplay, Wrapper, SliderTrack, SliderThumb };
+

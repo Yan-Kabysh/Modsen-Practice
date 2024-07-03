5094e3458d9e0cafbdc31b012d6d53bfc30910de
abamka <Januska12>
2024-07-03 17:11:55 +0300

    add git course

--- codewars/FindTheUniqueNumber.js
+++
@@ -1,3 +0,0 @@
-function findUniq(arr) {
-    return arr[0] === arr[1] ? arr.find(elem => elem !== arr[0]) : (arr[1] === arr[2] ? arr[0] : arr[1])
-  }
--- codewars/ThrowingDarts.js
+++
@@ -1,6 +0,0 @@
-function scoreThrows(radii){
-    if(radii.length === 0) 
-      return 0
-    let result = radii.reduce((accum, elem) => elem > 10 ? accum : (elem >= 5 ? accum + 5 : accum + 10), 0)
-    return result / 10 === radii.length ? result + 100 : result
-  }

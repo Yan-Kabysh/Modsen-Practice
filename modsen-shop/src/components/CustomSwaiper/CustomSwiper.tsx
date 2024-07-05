b8efa7dca6beedaa3761ca16fb1350c402dfa4de
abamka <Januska12>
2024-07-05 12:44:41 +0300

    fix: merge problem

--- modsen-shop/src/components/CustomSwaiper/CustomSwiper.tsx
+++ modsen-shop/src/components/CustomSwaiper/CustomSwiper.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
 import { CustomSwiperProps } from '@/../types/types';
-import { slides } from './Slides';
 
 import {
   Pagination,
@@ -10,6 +9,22 @@ import {
   SlideContainer,
   SliderWrapper,
 } from './StyledCustomSwiper';
+
+import { Img } from '@/components/CustomSwaiper/StyledCustomSwiper';
+import blackAndWhiteImage from '@/images/black-white.jpg';
+import natureImage from '@/images/nature.jpg';
+import tuscanyImage from '@/images/tuscany.jpg';
+const slides = [
+  <div key="slide1">
+    <Img src={blackAndWhiteImage} alt="" />
+  </div>,
+  <div key="slide2">
+    <Img src={natureImage} alt="" />
+  </div>,
+  <div key="slide3">
+    <Img src={tuscanyImage} alt="" />
+  </div>,
+];
 
 const CustomSwiper: React.FC<CustomSwiperProps> = ({
   autoScrollInterval = 3000,
